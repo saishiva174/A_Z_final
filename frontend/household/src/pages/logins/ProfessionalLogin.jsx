573579038025fd53bf7621bdc186bdcddfce6901
saishiva174 <gajjelasaishivareddy174@gmail.com>
2026-03-11 01:00:34 +0530

    case senstivity for vercel1

--- frontend/household/src/pages/logins/ProfessionalLogin.jsx
+++ frontend/household/src/pages/logins/ProfessionalLogin.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { FiLock, FiLogIn, FiArrowLeft, FiUser, FiEye, FiEyeOff } from 'react-icons/fi';
-import './ProfessionalSignup.css'; // Using the same CSS file for consistency
+import './ProfessionalSignUp.css'; // Using the same CSS file for consistency
 import { API_URL } from '../../apiConfig';
 
 const ProfessionalLogin = () => {
